1d405eeba42411dfca3b79e5e56b88214259f449
nadav2051 <nadav2051@gmail.com>
2015-05-05 22:46:24 +0300

    Hello uses Greeter class.

--- lib/hello.py
+++ lib/hello.py
@@ -1,7 +1,9 @@
 # Author: Nadav Luzzato (nadav2051@gmail.com)
 import sys
+from greeter import Greeter
 name = "World"
 # Comment for git
 if (len(sys.argv) > 1):
 	name = str(sys.argv[1])
-print "Hello " + name + "!"
+greeter = Greeter(name)
+greeter.greet()
